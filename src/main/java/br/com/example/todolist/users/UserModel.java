4f2da587f60c77644f20db22747cec478f935e52
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-10 14:57:53 -0300

    ✨ feat: implementa lombok na camada model

--- src/main/java/br/com/example/todolist/users/UserModel.java
+++ src/main/java/br/com/example/todolist/users/UserModel.java
@@ -1,35 +1,12 @@
 package br.com.example.todolist.users;
 
+import lombok.Data;
+
+@Data // cria os getters e setters para todos os atributos
 public class UserModel {
 
   private String username;
   private String name;
   private String password;
-
-  // atribui ao modificador privado o valor que vem do serUsername
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  // retorna o valor do modificador privado
-  public String getUsername() {
-    return username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 
 }
