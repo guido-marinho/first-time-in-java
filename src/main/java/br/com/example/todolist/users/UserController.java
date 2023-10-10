199b61c33db999e55e146221869ce481c43cd086
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-10 14:34:45 -0300

    ✨ feat: implementa rota post
    - Implementa rota post na camada de controller
    - implementa camada de model para rota post

---
+++ src/main/java/br/com/example/todolist/users/UserController.java
@@ -0,0 +1,43 @@
+package br.com.example.todolist.users;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// o nome da classe precisa ser o  msm nome do arquivo
+// começa sempre com letra maiuscula
+/**
+ * Modificador:
+ * public: pode ser acessado por qualquer classe
+ * private: só pode ser acessado pela classe
+ * protected: pode ser acessado pela classe e pelas classes filhas
+ */
+
+// anotação para indicar que é um controller
+@RestController
+// anotação para indicar o caminho da url (localhost:8080/users)
+@RequestMapping("/users")
+public class UserController {
+
+  // metodo de cadastro
+  /*
+   * principais tipos de dados:
+   * String
+   * Intenger
+   * Double
+   * Float
+   * char
+   * Date
+   * Void
+   */
+
+  // anotação para indicar que é um metodo post
+  @PostMapping("/")
+  // modificador -> tipo de retorno -> nome do metodo -> parametros (definindo que
+  // as informações vão vir no corpo da requisição)
+  public void create(@RequestBody UserModel userModel) {
+    // prntar no terminal
+    System.out.println(userModel.username);
+  }
+}
---
+++ src/main/java/br/com/example/todolist/users/UserModel.java
@@ -0,0 +1,9 @@
+package br.com.example.todolist.users;
+
+public class UserModel {
+
+  public String username;
+  public String name;
+  public String password;
+
+}
