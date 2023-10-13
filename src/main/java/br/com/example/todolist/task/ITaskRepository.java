2b12a3e4b84bd02201009de8d69dcf36df8e9ac1
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-13 16:15:21 -0300

    ✨ feat: implementa get para todas as tarefas

--- src/main/java/br/com/example/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/example/todolist/task/ITaskRepository.java
@@ -1,10 +1,11 @@
 package br.com.example.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // extende o jpa passando qual vai ser a entidade e qual vai sero id
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-
+  List<TaskModel> findByIdUser(UUID idUser);
 }
--- src/main/java/br/com/example/todolist/task/TaskController.java
+++ src/main/java/br/com/example/todolist/task/TaskController.java
@@ -1,10 +1,12 @@
 package br.com.example.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +29,21 @@ public class TaskController {
 
     var currentDate = LocalDateTime.now();
     if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
-      return ResponseEntity.status(401).body("Data de inicio / data de termino não pode ser menor que a data atual");
+      return ResponseEntity.status(401).body("Data de início/data de término não pode ser menor que a data atual");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(401).body("Data de início deve ser menor que a data de término");
     }
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(200).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
   }
 }
