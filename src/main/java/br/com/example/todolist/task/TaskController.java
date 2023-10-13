28be0e2847adf65840dc8f23c61ffbe82b49d8c9
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-13 16:03:09 -0300

    ✨ feat: implementa validação de data

--- src/main/java/br/com/example/todolist/task/TaskController.java
+++ src/main/java/br/com/example/todolist/task/TaskController.java
@@ -1,8 +1,10 @@
 package br.com.example.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,17 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(401).body("Data de inicio / data de termino não pode ser menor que a data atual");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(200).body(task);
   }
 }
